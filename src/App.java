31efeb6d90dc553d775d83a2e2ca1447a1897dcd
d-aggarwal <drishtiaggarwal6284@gmail.com>
2024-03-30 19:03:55 +0530

    player and board package added

---
+++ src/App.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+import board.Board;
+import player.Player;
+
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+        Board b = new Board(4,'&');
+        System.out.println("please enter name of player 1");
+        String name = sc.next();
+        System.out.println("please enter symbol of player 1");
+        char symbol = sc.nextLine().charAt(0);
+        Player p1 = new Player();
+        p1.setPlayerAndSymbol(name,symbol);
+        System.out.println("please enter name of player 2");
+        String name2 = sc.next();
+        System.out.println("please enter symbol of player 2");
+        char symbol2 = sc.nextLine().charAt(0);
+        Player p2 = new Player();
+        p2.setPlayerAndSymbol(name2,symbol2);
+
+
+
+
+    }
+
+}
+
+
+
---
+++ src/player/Player.java
@@ -0,0 +1,59 @@
+package player;
+
+public class Player {
+    private String name;
+    private char symbol;
+    private String address;
+    private String contactnumber;
+    private String emailid;
+    private int age;
+
+    public void setPlayerDetails(String name,char symbol,String address,String contactnumber, String emailid,int age) {
+        this.name = name;
+        this.symbol = symbol;
+        this.address = address;
+        this.contactnumber = contactnumber;
+        this.emailid = emailid;
+        this.age = age;
+
+    }
+
+    public void setPlayerNameAndAge(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setPlayerNameSymbolEmailAge(String name, char symbol, String emailid, int age) {
+        this.name = name;
+        this.age = age;
+        this.emailid = emailid;
+        this.symbol = symbol;
+    }
+
+    public void changesymbol(char symbol ){
+        this.symbol = symbol;
+    }
+
+    public void setPlayerAndSymbol(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getPlayerName() {
+        return this.name;
+    }
+
+    public void getPlayerNameAndSymbol() {
+        System.out.println("Player Name: "+ this.name);
+        System.out.println("Player Symbol: "+ this.symbol);
+    }
+
+    public void getPlayerDetails() {
+        System.out.println("Player Name: "+ this.name);
+        System.out.println("Player Symbol: "+ this.symbol);
+        System.out.println("Player Email: "+ this.emailid);
+        System.out.println("Player Age: "+ this.age);
+        System.out.println("Player Contact Number: "+ this.contactnumber);
+        System.out.println("Player Address: "+ this.address);
+    }
+}
